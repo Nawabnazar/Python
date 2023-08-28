2a27096810942e8d3aab380ed1837e6e069591ce
Nawabnazar <142574177+Nawabnazar@users.noreply.github.com>
2023-08-28 15:20:21 +0530

    Add files via upload
    The file make to easy way to sreach to your record

---
+++ binary search.py
@@ -0,0 +1,21 @@
+# There we start making the binary screach program
+a=[-1,0,3,5,9,12,15]
+target = 17
+len= len(a)
+len = int(len/2)
+if a[len] == target:
+    print(len)
+elif a[len] < target:
+    while(1):
+        len = len +1
+        if a[len] == target:
+            print(len)
+            exit()
+if len > target:
+    while(1):
+        len = len -1
+        if a[len] == target:
+            print(len)
+            exit()
+else:
+    print(-1)
